dce0db26216df8b0577649caffd757c2bb18bfb5
Hafiz-Huzaifa-dev <mh4818438@gmail.com>
2026-01-09 14:12:22 +0500

    adding a vedor dashbaord page

---
+++ app/Vendor-dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1 className='font-bold flex justify-center m-10'>Wellcome to Vendor Dashbaord</h1>
+    </div>
+  )
+}
+
+export default page

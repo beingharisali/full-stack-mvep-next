63e9de171fc4e21c56765cea50383bdde80e483d
Hafiz-Huzaifa-dev <mh4818438@gmail.com>
2026-01-09 14:27:56 +0500

    add navbar in vendor dashbaord

--- app/Vendor-dashboard/page.tsx
+++ app/Vendor-dashboard/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function page() {
   return (
     <div>
+      <Navbar/>
       <h1 className='font-bold flex justify-center m-10'>Wellcome to Vendor Dashbaord</h1>
     </div>
   )

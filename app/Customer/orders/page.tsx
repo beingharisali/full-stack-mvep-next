e5de6e4f5f1cafd108ad3741348fefd86b49d95c
Fatima <bc220417002fzn@vu.edu.pk>
2026-02-19 19:45:36 +0500

    ....

--- app/Customer/orders/page.tsx
+++ app/Customer/orders/page.tsx
@@ -63,7 +63,7 @@ const OrdersPage: React.FC = () => {
   if (loading) {
     return (
       <ProtectedRoute allowedRoles={['customer']} redirectPath="/">
-        <div className="min-h-screen bg-[#050a14]">
+        <div className="min-h-screen">
           <Navbar onMenuToggle={toggleSidebar} sidebarOpen={sidebarOpen} />
           <div className="flex">
             <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />

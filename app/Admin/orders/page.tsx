8a890b6e0d8d641d65d21dcd6492afd03c90381e
Fatima <bc220417002fzn@vu.edu.pk>
2026-02-19 19:40:44 +0500

    changes in background of admin orders page

--- app/Admin/orders/page.tsx
+++ app/Admin/orders/page.tsx
@@ -142,7 +142,7 @@ const AdminOrdersPage: React.FC = () => {
 
   return (
     <ProtectedRoute allowedRoles={["admin"]} redirectPath="/">
-      <div className="min-h-screen bg-[#050a14]">
+      <div className="min-h-screen">
         <Navbar onMenuToggle={toggleSidebar} sidebarOpen={sidebarOpen} />
         <div className="flex">
           <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />

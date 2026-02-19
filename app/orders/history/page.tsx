dbeecbe8bf8ce65fe94ff3836ce1912f19cd2613
Fatima <bc220417002fzn@vu.edu.pk>
2026-02-19 19:48:13 +0500

    further changes

--- app/orders/history/page.tsx
+++ app/orders/history/page.tsx
@@ -121,7 +121,7 @@ const OrderHistoryPage: React.FC = () => {
 
   return (
     <ProtectedRoute allowedRoles={['customer']} redirectPath="/">
-      <div className="min-h-screen bg-[#050a14]">
+      <div className="min-h-screen">
         <Navbar onMenuToggle={toggleSidebar} sidebarOpen={sidebarOpen} />
         <div className="flex">
           <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
